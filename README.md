c736c82547f60a1e16f7384e8611d89e6f724b0d
gvg-job4j <gvgjava2018@yandex.ru>
2018-08-15 00:18:12 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Репозиторий Валерия Гурьевских
+[![Build Status](https://travis-ci.org/gvg-job4j/job4j.svg?branch=master)](https://travis-ci.org/gvg-job4j/job4j)
+[![codecov](https://codecov.io/gh/gvg-job4j/job4j/branch/master/graph/badge.svg)](https://codecov.io/gh/gvg-job4j/job4j)
+
+Я прохожу обучение по курсу Job4j (http://job4j.ru/). Планирую его пройти к концу 2018 года.
+Ниже находятся наиболее интересные проекты, которые я реализовал во время обучения.
---
+++ chapter_001/src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -0,0 +1,77 @@
+package ru.job4j.iterator;
+
+import java.util.*;
+import java.lang.Integer;
+
+/**
+ * @author Valeriy Gyrievskikh
+ * Итератор для двухмерного массива
+ * @since 09.08.2018
+ */
+public class MatrixIterator implements Iterator<Integer> {
+
+    /**
+     * Обрабатываемый массив.
+     */
+    private int[][] array;
+    /**
+     * Текущая строка массива.
+     */
+    private int currentRow;
+    /**
+     * Текущая колонка массива.
+     */
+    private int currentColumn;
+    /**
+     * Текущий индекс.
+     */
+    private int currentIndex;
+    /**
+     * Количество элементов в массиве.
+     */
+    private long size;
+
+    /**
+     * Конструктор класса.
+     *
+     * @param array
+     */
+    public MatrixIterator(int[][] array) {
+        this.array = array;
+        this.currentRow = 0;
+        this.currentColumn = 0;
+        this.currentIndex = 0;
+        this.size = Arrays.stream(array).flatMapToInt(x -> Arrays.stream(x)).count();
+    }
+
+    /**
+     * Метод проверяет, есть ли следующий элемент.
+     *
+     * @return Результат проверки.
+     */
+    @Override
+    public boolean hasNext() {
+        return this.currentIndex < this.size;
+    }
+
+    /**
+     * Метод возвращает следующий элемент массива.
+     *
+     * @return Элемент массива.
+     */
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        int element = array[currentRow][currentColumn];
+        if (array[currentRow].length == currentColumn + 1) {
+            currentRow++;
+            currentColumn = 0;
+        } else {
+            currentColumn++;
+        }
+        this.currentIndex++;
+        return element;
+    }
+}
