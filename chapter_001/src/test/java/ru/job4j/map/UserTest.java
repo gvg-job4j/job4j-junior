6e36a027ab2a96c3153815f5af8de8ec83eafba0
Valeriy Gyrievskikh <gvg2000@yandex.ru>
2018-09-26 07:00:23 +0300

    5.2. Не перекрывать equals hashCode.

---
+++ chapter_001/src/test/java/ru/job4j/map/UserTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.map;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Valeriy Gyrievskikh
+ * @since 25.09.2018
+ */
+public class UserTest {
+
+    @Test
+    public void createTwoUsersMap() {
+        Calendar birthday = new GregorianCalendar(2000, 01, 01);
+        Map<User, Object> userMap = new HashMap<>();
+        User user1 = new User("Vova", 1, birthday);
+        User user2 = new User("Vova", 1, birthday);
+        userMap.put(user1, new Object());
+        userMap.put(user2, new Object());
+        System.out.println(userMap.toString());
+    }
+}
