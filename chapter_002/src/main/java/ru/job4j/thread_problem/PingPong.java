fb80aac0b3f4bde2da3aefac8c7e9fe94b8d8e0f
gvg-github <gvg2000@yandex.ru>
2018-12-07 23:55:22 +0300

    1. Проиллюстрировать проблемы с многопоточностью.

---
+++ chapter_002/src/main/java/ru/job4j/thread_problem/PingPong.java
@@ -0,0 +1,19 @@
+package ru.job4j.thread_problem;
+
+public class PingPong {
+
+    public synchronized void closeThreads() {
+        notifyAll();
+    }
+
+    //        public synchronized void printString(String string) {
+    public void printString(String string) {
+        System.out.println("Thread " + Thread.currentThread().getName() + " print: " + string);
+        notify();
+        try {
+            wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ chapter_002/src/test/java/thread_problem/SimplePingPongTest.java
@@ -0,0 +1,31 @@
+package thread_problem;
+
+import org.junit.Test;
+import ru.job4j.thread_problem.PingPong;
+
+public class SimplePingPongTest {
+
+    @Test
+    public void startPingPong() {
+
+        PingPong printer = new PingPong();
+        Thread threadPing = new Thread(() -> {
+            int count = 0;
+            while (count < 20) {
+                printer.printString("Ping");
+                count++;
+            }
+            printer.closeThreads();
+        });
+        Thread threadPong = new Thread(() -> {
+            int count = 0;
+            while (count < 20) {
+                printer.printString("Pong");
+                count++;
+            }
+            printer.closeThreads();
+        });
+        threadPing.start();
+        threadPong.start();
+    }
+}
