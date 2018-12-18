e44219f8a189abefe2521ea6e56a52da202f02d4
gvg-github <gvg2000@yandex.ru>
2018-12-18 22:19:23 +0300

    1. класс Count и метод int incremant().

---
+++ chapter_002/src/main/java/ru/job4j/monitore/Count.java
@@ -0,0 +1,34 @@
+package ru.job4j.monitore;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+/**
+ * @author Valeriy Gyrievskikh
+ * Реализация многопоточного счетчика.
+ * @since 18.12.2018
+ */
+@ThreadSafe
+public class Count {
+    /**
+     * Значение счетчика.
+     */
+    @GuardedBy("this")
+    private int value;
+
+    /**
+     * Метод возвращает текущее значение счетчика.
+     *
+     * @return
+     */
+    public synchronized int incremant() {
+        return this.value;
+    }
+
+    /**
+     * Метод выполняет увеличение текущего значения счетчика на 1.
+     */
+    public synchronized void increment() {
+        this.value++;
+    }
+}
---
+++ chapter_002/src/test/java/monitore/CountTest.java
@@ -0,0 +1,54 @@
+package monitore;
+
+import ru.job4j.monitore.Count;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class CountTest {
+    /**
+     * Класс описывает нить со счетчиком.
+     */
+    private class ThreadCount extends Thread {
+        private final Count count;
+
+        private ThreadCount(final Count count) {
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            this.count.increment();
+        }
+    }
+
+    @Test
+    public void whenExecute2ThreadThen2() throws InterruptedException {
+        //Создаем счетчик.
+        final Count count = new Count();
+        //Создаем нити.
+        Thread first = new ThreadCount(count);
+        Thread second = new ThreadCount(count);
+        Thread third = new ThreadCount(count);
+        Thread fourth = new ThreadCount(count);
+        Thread fifth = new ThreadCount(count);
+        Thread sixth = new ThreadCount(count);
+        //Запускаем нити.
+        first.start();
+        second.start();
+        third.start();
+        fourth.start();
+        fifth.start();
+        sixth.start();
+        //Заставляем главную нить дождаться выполнения наших нитей.
+        first.join();
+        second.join();
+        third.join();
+        fourth.join();
+        fifth.join();
+        sixth.join();
+        //Проверяем результат.
+        assertThat(count.incremant(), is(6));
+    }
+}
