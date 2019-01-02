0b5c0b57e3f7d5373b348b3d299255d8a1822318
gvg-github <gvg2000@yandex.ru>
2019-01-02 16:59:21 +0300

    2. Удаление запрещенных слов.

--- chapter_002/src/main/java/ru/job4j/IO/ByteInput.java
+++ chapter_002/src/main/java/ru/job4j/IO/ByteInput.java
@@ -1,7 +1,6 @@
 package ru.job4j.IO;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 /**
  * @author Valeriy Gyrievskikh
@@ -10,6 +9,7 @@ import java.io.InputStream;
 public class ByteInput {
     /**
      * Метод проверяет, что в байтовом потоке записано четное число.
+     *
      * @param in Входной байтовый поток.
      * @return Результат проверки.
      */
@@ -19,15 +19,54 @@ public class ByteInput {
             StringBuilder bytes = new StringBuilder();
             int a = 0;
             while ((a = input.read()) != -1) {
-                System.out.println(a);
                 bytes.append(a);
             }
-            if (Integer.parseInt( bytes.toString()) % 2 == 0) {
+            if (Integer.parseInt(bytes.toString()) % 2 == 0) {
                 isEven = true;
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
         return isEven;
+    }
+
+    /**
+     * Метод записывает в выходной поток только те слова, которых нет в переданном массиве.
+     *
+     * @param in    Входной поток со списком слов.
+     * @param out   Выходной поток.
+     * @param abuse Массив с запрещенными словами.
+     */
+    void dropAbuses(InputStream in, OutputStream out, String[] abuse) {
+        try (InputStream input = in
+        ) {
+            StringBuilder bytes = new StringBuilder();
+            int a = 0;
+            while ((a = input.read()) != -1) {
+                if (a == 32) {
+                    boolean isAbuse = false;
+                    for (int i = 0; i < abuse.length; i++) {
+                        if (abuse[i].equals(bytes.toString())) {
+                            isAbuse = true;
+                            break;
+                        }
+                    }
+                    if (!isAbuse) {
+                        bytes.appendCodePoint(a);
+                        out.write(bytes.toString().getBytes());
+                        out.flush();
+                    }
+                    bytes = new StringBuilder();
+                } else {
+                    bytes.appendCodePoint(a);
+                }
+            }
+            if (bytes.length() > 0) {
+                out.write(bytes.toString().getBytes());
+                out.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- chapter_002/src/test/java/ru/job4j/IO/ByteInputTest.java
+++ chapter_002/src/test/java/ru/job4j/IO/ByteInputTest.java
@@ -7,10 +7,8 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.hamcrest.core.Is.is;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author Valeriy Gyrievskikh
@@ -19,17 +17,38 @@ import java.io.ObjectInputStream;
 public class ByteInputTest {
 
     @Test
-    public void whenByteArrayEvenNumberThenTrue(){
+    public void whenByteArrayEvenNumberThenTrue() {
         InputStream in = new ByteArrayInputStream(new byte[]{1, 2});
         ByteInput input = new ByteInput();
         assertTrue(input.isNumber(in));
     }
 
     @Test
-    public void whenNotByteArrayEvenNumberThenFalse(){
+    public void whenNotByteArrayEvenNumberThenFalse() {
         InputStream in = new ByteArrayInputStream(new byte[]{1, 2, 1, 1});
         ByteInput input = new ByteInput();
         assertNotEquals(input.isNumber(in), true);
     }
 
+    @Test
+    public void whenInputHaveAbuseWorldsThenOutputDonHave() {
+        String myStrObj = "Hello World Its Java!";
+        String[] abuse = new String[]{"Hello", "Its"};
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ByteArrayInputStream bais = new ByteArrayInputStream(myStrObj.getBytes());
+        ByteInput input = new ByteInput();
+        input.dropAbuses(bais, baos, abuse);
+        assertThat(baos.toString(), is("World Java!"));
+    }
+
+    @Test
+    public void whenInputDontHaveAbuseWorldsThenOutputAll() {
+        String myStrObj = "Hello World Its Java!";
+        String[] abuse = new String[]{"HelloWorld", "ItsJava"};
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ByteArrayInputStream bais = new ByteArrayInputStream(myStrObj.getBytes());
+        ByteInput input = new ByteInput();
+        input.dropAbuses(bais, baos, abuse);
+        assertThat(baos.toString(), is("Hello World Its Java!"));
+    }
 }
