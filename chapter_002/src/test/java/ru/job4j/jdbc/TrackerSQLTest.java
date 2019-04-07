13571100a92aa150118a2ffad0d6dc6624064b0e
gvg-github <gvg2000@yandex.ru>
2019-04-07 09:46:31 +0300

    Вернул тесты для TrackerSQL.

--- chapter_002/src/test/java/ru/job4j/jdbc/TrackerSQLTest.java
+++ chapter_002/src/test/java/ru/job4j/jdbc/TrackerSQLTest.java
@@ -1,12 +1,14 @@
 package ru.job4j.jdbc;
 
 import org.hamcrest.core.Is;
+import org.junit.Before;
 import org.junit.Test;
 import ru.job4j.tracker.Item;
 
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.List;
 import java.util.Properties;
 
 import static org.junit.Assert.*;
@@ -18,28 +20,42 @@ import static org.hamcrest.Matchers.is;
  */
 public class TrackerSQLTest {
 
-    private Connection init() {
-        try (InputStream in = TrackerSQL.class.getClassLoader().getResourceAsStream("app.properties")) {
-            Properties config = new Properties();
-            config.load(in);
-            Class.forName(config.getProperty("driver-class-name"));
-            return DriverManager.getConnection(
-                    config.getProperty("url"),
-                    config.getProperty("username"),
-                    config.getProperty("password")
+    private TrackerSQL tracker;
 
-            );
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
+    @Before
+    public void initTracker() {
+        tracker = new TrackerSQL();
+        tracker.init();
+        List<Item> currentRequests = tracker.findAll();
+        if (currentRequests != null) {
+            for (Item item : currentRequests) {
+                tracker.delete(item.getId());
+            }
         }
     }
 
+//    private Connection init() {
+//        try (InputStream in = TrackerSQL.class.getClassLoader().getResourceAsStream("app.properties")) {
+//            Properties config = new Properties();
+//            config.load(in);
+//            Class.forName(config.getProperty("driver-class-name"));
+//            return DriverManager.getConnection(
+//                    config.getProperty("url"),
+//                    config.getProperty("username"),
+//                    config.getProperty("password")
+//
+//            );
+//        } catch (Exception e) {
+//            throw new IllegalStateException(e);
+//        }
+//    }
+
     @Test
     public void createItem() throws Exception {
-        try (TrackerSQL tracker = new TrackerSQL(ConnectionRollback.create(this.init()))) {
+//        try (TrackerSQL tracker = new TrackerSQL(ConnectionRollback.create(this.init()))) {
             tracker.add(new Item("name", "testDescription", 123L));
             assertThat(tracker.findByName("name").size(), is(1));
-        }
+//        }
     }
 
     @Test
@@ -57,16 +73,16 @@ public class TrackerSQLTest {
 
     @Test
     public void whenAddNewItemThenTrackerHasSameItem() throws Exception {
-        try (TrackerSQL tracker = new TrackerSQL(ConnectionRollback.create(this.init()))) {
+//        try (TrackerSQL tracker = new TrackerSQL(ConnectionRollback.create(this.init()))) {
             Item item = new Item("test1", "testDescription", 123L);
             tracker.add(item);
             assertThat(tracker.findAll().get(0).getId(), Is.is(item.getId()));
-        }
+//        }
     }
 
     @Test
     public void whenReplaceNameThenReturnNewName() throws Exception {
-        try (TrackerSQL tracker = new TrackerSQL(ConnectionRollback.create(this.init()))) {
+//        try (TrackerSQL tracker = new TrackerSQL(ConnectionRollback.create(this.init()))) {
             Item previous = new Item("test1", "testDescription", 123L);
             tracker.add(previous);
             previous.setId(Long.toString(previous.getNumber()));
@@ -74,12 +90,12 @@ public class TrackerSQLTest {
             next.setId(Long.toString(next.getNumber()));
             tracker.replace(previous.getId(), next);
             assertThat(tracker.findById(previous.getId()).getName(), Is.is("test2"));
-        }
+//        }
     }
 
     @Test
     public void whenDeleteItemThenReturnNull() throws Exception {
-        try (TrackerSQL tracker = new TrackerSQL(ConnectionRollback.create(this.init()))) {
+//        try (TrackerSQL tracker = new TrackerSQL(ConnectionRollback.create(this.init()))) {
             Item newItem1 = new Item("test1", "testDescription", 123L);
             Item newItem2 = new Item("test2", "testDescription", 1234L);
             Item newItem3 = new Item("test3", "testDescription", 12345L);
@@ -91,21 +107,21 @@ public class TrackerSQLTest {
             tracker.add(newItem3);
             tracker.delete(newItem1.getId());
             assertNull(tracker.findById(newItem1.getId()));
-        }
+//        }
     }
 
     @Test
     public void whenFindAllThenReturnArray() throws Exception {
-        try (TrackerSQL tracker = new TrackerSQL(ConnectionRollback.create(this.init()))) {
+//        try (TrackerSQL tracker = new TrackerSQL(ConnectionRollback.create(this.init()))) {
             Item newItem = new Item("test1", "testDescription", 123L);
             tracker.add(newItem);
             assertThat(tracker.findAll().size(), Is.is(1));
-        }
+//        }
     }
 
     @Test
     public void whenFindByNameThenReturnArrayLength() throws Exception {
-        try (TrackerSQL tracker = new TrackerSQL(ConnectionRollback.create(this.init()))) {
+//        try (TrackerSQL tracker = new TrackerSQL(ConnectionRollback.create(this.init()))) {
             Item newItem1 = new Item("test1", "testDescription", 123L);
             Item newItem2 = new Item("test1", "testDescription", 1234L);
             Item newItem3 = new Item("test3", "testDescription", 123L);
@@ -113,15 +129,15 @@ public class TrackerSQLTest {
             tracker.add(newItem2);
             tracker.add(newItem3);
             assertThat(tracker.findByName("test1").size(), Is.is(2));
-        }
+//        }
     }
 
     @Test
     public void whenFindByIdThenReturnItem() throws Exception {
-        try (TrackerSQL tracker = new TrackerSQL(ConnectionRollback.create(this.init()))) {
+//        try (TrackerSQL tracker = new TrackerSQL(ConnectionRollback.create(this.init()))) {
             Item newItem = new Item("test1", "testDescription", 123L);
             tracker.add(newItem);
             assertThat(tracker.findById(newItem.getId()).getId(), Is.is(newItem.getId()));
-        }
+//        }
     }
 }
