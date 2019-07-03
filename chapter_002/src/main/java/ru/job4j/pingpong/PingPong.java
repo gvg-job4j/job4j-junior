0318e8081dd108c595bda7827dc1f1f121773a39
gvg-github <gvg2000@yandex.ru>
2019-07-03 07:31:34 +0300

    Проверка Travis

--- chapter_002/src/main/java/ru/job4j/pingpong/PingPong.java
+++ chapter_002/src/main/java/ru/job4j/pingpong/PingPong.java
@@ -1,39 +1,40 @@
 package ru.job4j.pingpong;
 
-import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
+//import javafx.application.Application;
+//import javafx.scene.Group;
+//import javafx.scene.Scene;
+//import javafx.scene.shape.Rectangle;
+//import javafx.stage.Stage;
 
 /**
  * @author Valeriy Gyrievskikh
  * @since 22.11.2018
  */
-public class PingPong extends Application {
-    /**
-     * Заголовок формы.
-     */
-    private static final String JOB4J = "Пинг-понг www.job4j.ru";
-
-    /**
-     * Метод выполняет запуск программы и открывает рабочее окно.
-     *
-     * @param stage Окно программы.
-     */
-    @Override
-    public void start(Stage stage) {
-        int limitX = 300;
-        int limitY = 300;
-        Group group = new Group();
-        Rectangle rect = new Rectangle(50, 100, 10, 10);
-        group.getChildren().add(rect);
-        Thread pingPongThread = new Thread(new RectangleMove(rect, limitX, limitY));
-        pingPongThread.start();
-        stage.setScene(new Scene(group, limitX, limitY));
-        stage.setTitle(JOB4J);
-        stage.setResizable(false);
-        stage.setOnCloseRequest(event -> pingPongThread.interrupt());
-        stage.show();
-    }
+public class PingPong {
+//public class PingPong extends Application {
+//    /**
+//     * Заголовок формы.
+//     */
+//    private static final String JOB4J = "Пинг-понг www.job4j.ru";
+//
+//    /**
+//     * Метод выполняет запуск программы и открывает рабочее окно.
+//     *
+//     * @param stage Окно программы.
+//     */
+//    @Override
+//    public void start(Stage stage) {
+//        int limitX = 300;
+//        int limitY = 300;
+//        Group group = new Group();
+//        Rectangle rect = new Rectangle(50, 100, 10, 10);
+//        group.getChildren().add(rect);
+//        Thread pingPongThread = new Thread(new RectangleMove(rect, limitX, limitY));
+//        pingPongThread.start();
+//        stage.setScene(new Scene(group, limitX, limitY));
+//        stage.setTitle(JOB4J);
+//        stage.setResizable(false);
+//        stage.setOnCloseRequest(event -> pingPongThread.interrupt());
+//        stage.show();
+//    }
 }
--- chapter_002/src/main/java/ru/job4j/pingpong/RectangleMove.java
+++ chapter_002/src/main/java/ru/job4j/pingpong/RectangleMove.java
@@ -1,16 +1,17 @@
 package ru.job4j.pingpong;
 
-import javafx.scene.shape.Rectangle;
+//import javafx.scene.shape.Rectangle;
 
 /**
  * @author Valeriy Gyrievskikh
  * @since 22.11.2018
  */
-public class RectangleMove implements Runnable {
-    /**
-     * Перемещаемая фигура.
-     */
-    private final Rectangle rect;
+public class RectangleMove {
+//public class RectangleMove implements Runnable {
+//    /**
+//     * Перемещаемая фигура.
+//     */
+//    private final Rectangle rect;
     /**
      * Максимальный размер по Х.
      */
@@ -28,38 +29,38 @@ public class RectangleMove implements Runnable {
      */
     private int deltaY = 5;
 
-    /**
-     * Конструктор, задаеет начальные значения.
-     *
-     * @param rect Перемещаемая фигура.
-     * @param maxX Максимальный размер по Х.
-     * @param maxY Максимальный размер по Y.
-     */
-    public RectangleMove(Rectangle rect, int maxX, int maxY) {
-        this.rect = rect;
-        this.maxX = maxX;
-        this.maxY = maxY;
-    }
-
-    /**
-     * Метод выполняет изменение координат фигуры с течением времени.
-     */
-    @Override
-    public void run() {
-        while (!Thread.currentThread().isInterrupted()) {
-            try {
-                this.rect.setX(this.rect.getX() + deltaX);
-                this.rect.setY(this.rect.getY() + deltaY);
-                Thread.sleep(50);
-                if (this.rect.getX() >= maxX || this.rect.getX() <= 0) {
-                    deltaX = -deltaX;
-                }
-                if (this.rect.getY() >= maxY || this.rect.getY() <= 0) {
-                    deltaY = -deltaY;
-                }
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }
-    }
+//    /**
+//     * Конструктор, задаеет начальные значения.
+//     *
+//     * @param rect Перемещаемая фигура.
+//     * @param maxX Максимальный размер по Х.
+//     * @param maxY Максимальный размер по Y.
+//     */
+//    public RectangleMove(Rectangle rect, int maxX, int maxY) {
+//        this.rect = rect;
+//        this.maxX = maxX;
+//        this.maxY = maxY;
+//    }
+//
+//    /**
+//     * Метод выполняет изменение координат фигуры с течением времени.
+//     */
+//    @Override
+//    public void run() {
+//        while (!Thread.currentThread().isInterrupted()) {
+//            try {
+//                this.rect.setX(this.rect.getX() + deltaX);
+//                this.rect.setY(this.rect.getY() + deltaY);
+//                Thread.sleep(50);
+//                if (this.rect.getX() >= maxX || this.rect.getX() <= 0) {
+//                    deltaX = -deltaX;
+//                }
+//                if (this.rect.getY() >= maxY || this.rect.getY() <= 0) {
+//                    deltaY = -deltaY;
+//                }
+//            } catch (InterruptedException e) {
+//                Thread.currentThread().interrupt();
+//            }
+//        }
+//    }
 }
