19faf9332cd8ecb0a82a4cb7eeaacd27be447038
gvg-github <gvg2000@yandex.ru>
2019-07-17 08:41:12 +0300

    Проверка Travis

--- chapter_002/src/test/java/ru/job4j/solid/lsp/ControlQualityEventLoopTest.java
+++ chapter_002/src/test/java/ru/job4j/solid/lsp/ControlQualityEventLoopTest.java
@@ -15,83 +15,83 @@ import static org.junit.Assert.assertThat;
  */
 public class ControlQualityEventLoopTest {
 
-//    private Warehouse warehouse = new Warehouse();
-//    private Thrash thrash = new Thrash();
-//    private Shop shop = new Shop();
-//    private LocalDate date = LocalDate.now();
-//    private ControlQuality cq = new ControlQuality();
-//
-//    @Before
-//    public void init() {
-//        cq.initHandler();
-//    }
-//
-//    @Test
-//    public void whenThresholdLess75ThenAddToStore() {
-//        Food bread1 = new Bread("bread 1", date.plusDays(10).toString(),
-//                date.minusDays(50).toString(), 50.00, 10);
-//        Food bread2 = new Bread("bread 2", date.plusDays(40).toString(),
-//                date.minusDays(50).toString(), 60.00, 15);
-//        Food milk1 = new Milk("milk 1", date.plusDays(5).toString(),
-//                date.minusDays(5).toString(), 45.00, 20);
-//        Food milk2 = new Milk("milk 2", date.plusDays(1).toString(),
-//                date.minusDays(5).toString(), 80.00, 30);
-//        cq.sendToStore(bread1);
-//        cq.sendToStore(bread2);
-//        cq.sendToStore(milk1);
-//        cq.sendToStore(milk2);
-//        for (FoodHandler handler : cq.getStores()) {
-//            if (handler instanceof Shop) {
-//                shop = (Shop) handler;
-//                break;
-//            }
-//        }
-//        assertThat(shop.getProducts().size(), is(4));
-//    }
-//
-//    @Test
-//    public void whenThresholdMore75ThenAddToWarehouse() {
-//        Food bread1 = new Bread("bread 1", date.plusDays(100).toString(),
-//                date.minusDays(10).toString(), 50.00, 10);
-//        Food bread2 = new Bread("bread 2", date.plusDays(80).toString(),
-//                date.minusDays(10).toString(), 60.00, 15);
-//        Food milk1 = new Milk("milk 1", date.plusDays(10).toString(),
-//                date.minusDays(1).toString(), 45.00, 20);
-//        Food milk2 = new Milk("milk 2", date.plusDays(8).toString(),
-//                date.minusDays(1).toString(), 80.00, 30);
-//        cq.sendToStore(bread1);
-//        cq.sendToStore(bread2);
-//        cq.sendToStore(milk1);
-//        cq.sendToStore(milk2);
-//        for (FoodHandler handler : cq.getStores()) {
-//            if (handler instanceof Warehouse) {
-//                warehouse = (Warehouse) handler;
-//                break;
-//            }
-//        }
-//        assertThat(warehouse.getProducts().size(), is(4));
-//    }
-//
-//    @Test
-//    public void whenThreshold0ThenAddToThrash() {
-//        Food bread1 = new Bread("bread 1", date.minusDays(1).toString(),
-//                date.minusDays(10).toString(), 50.00, 10);
-//        Food bread2 = new Bread("bread 2", date.minusDays(2).toString(),
-//                date.minusDays(10).toString(), 60.00, 15);
-//        Food milk1 = new Milk("milk 1", date.toString(),
-//                date.minusDays(2).toString(), 45.00, 20);
-//        Food milk2 = new Milk("milk 2", date.minusDays(1).toString(),
-//                date.minusDays(2).toString(), 80.00, 30);
-//        cq.sendToStore(bread1);
-//        cq.sendToStore(bread2);
-//        cq.sendToStore(milk1);
-//        cq.sendToStore(milk2);
-//        for (FoodHandler handler : cq.getStores()) {
-//            if (handler instanceof Thrash) {
-//                thrash = (Thrash) handler;
-//                break;
-//            }
-//        }
-//        assertThat(thrash.getProducts().size(), is(4));
-//    }
+    private Warehouse warehouse = new Warehouse();
+    private Thrash thrash = new Thrash();
+    private Shop shop = new Shop();
+    private LocalDate date = LocalDate.now();
+    private ControlQuality cq = new ControlQuality();
+
+    @Before
+    public void init() {
+        cq.initHandler();
+    }
+
+    @Test
+    public void whenThresholdLess75ThenAddToStore() {
+        Food bread1 = new Bread("bread 1", date.plusDays(10).toString(),
+                date.minusDays(50).toString(), 50.00, 10);
+        Food bread2 = new Bread("bread 2", date.plusDays(40).toString(),
+                date.minusDays(50).toString(), 60.00, 15);
+        Food milk1 = new Milk("milk 1", date.plusDays(5).toString(),
+                date.minusDays(5).toString(), 45.00, 20);
+        Food milk2 = new Milk("milk 2", date.plusDays(1).toString(),
+                date.minusDays(5).toString(), 80.00, 30);
+        cq.sendToStore(bread1);
+        cq.sendToStore(bread2);
+        cq.sendToStore(milk1);
+        cq.sendToStore(milk2);
+        for (FoodHandler handler : cq.getStores()) {
+            if (handler instanceof Shop) {
+                shop = (Shop) handler;
+                break;
+            }
+        }
+        assertThat(shop.getProducts().size(), is(4));
+    }
+
+    @Test
+    public void whenThresholdMore75ThenAddToWarehouse() {
+        Food bread1 = new Bread("bread 1", date.plusDays(100).toString(),
+                date.minusDays(10).toString(), 50.00, 10);
+        Food bread2 = new Bread("bread 2", date.plusDays(80).toString(),
+                date.minusDays(10).toString(), 60.00, 15);
+        Food milk1 = new Milk("milk 1", date.plusDays(10).toString(),
+                date.minusDays(1).toString(), 45.00, 20);
+        Food milk2 = new Milk("milk 2", date.plusDays(8).toString(),
+                date.minusDays(1).toString(), 80.00, 30);
+        cq.sendToStore(bread1);
+        cq.sendToStore(bread2);
+        cq.sendToStore(milk1);
+        cq.sendToStore(milk2);
+        for (FoodHandler handler : cq.getStores()) {
+            if (handler instanceof Warehouse) {
+                warehouse = (Warehouse) handler;
+                break;
+            }
+        }
+        assertThat(warehouse.getProducts().size(), is(4));
+    }
+
+    @Test
+    public void whenThreshold0ThenAddToThrash() {
+        Food bread1 = new Bread("bread 1", date.minusDays(1).toString(),
+                date.minusDays(10).toString(), 50.00, 10);
+        Food bread2 = new Bread("bread 2", date.minusDays(2).toString(),
+                date.minusDays(10).toString(), 60.00, 15);
+        Food milk1 = new Milk("milk 1", date.toString(),
+                date.minusDays(2).toString(), 45.00, 20);
+        Food milk2 = new Milk("milk 2", date.minusDays(1).toString(),
+                date.minusDays(2).toString(), 80.00, 30);
+        cq.sendToStore(bread1);
+        cq.sendToStore(bread2);
+        cq.sendToStore(milk1);
+        cq.sendToStore(milk2);
+        for (FoodHandler handler : cq.getStores()) {
+            if (handler instanceof Thrash) {
+                thrash = (Thrash) handler;
+                break;
+            }
+        }
+        assertThat(thrash.getProducts().size(), is(4));
+    }
 }
